3ff4383aa12a28ae985542b19873f97675b669fd
Justin <theskaterjesus@gmail.com>
2023-08-03 20:41:53 +0100

    Created basic serviceprovider and autoload

---
+++ src/DocumentationServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace RexDevs\Documentation;
+
+use Illuminate\Support\ServiceProvider;
+
+class DocumentationServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
