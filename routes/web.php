8a0400587fe7b15141373354ec4f9de596c917b5
Justin <theskaterjesus@gmail.com>
2023-08-05 09:15:43 +0100

    Added routes to web file

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,10 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+use RexDevs\Documentation\Http\Controllers\DocumentationController;
+
+foreach (config('documentation.routes') as $routes) {
+    Route::get($route['url'].'/{file?}', DocumentationController::class)
+        ->where('file', '.+')
+        ->name($route['name']);
+}
