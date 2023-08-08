c329d6ae77a81f7c75852dc7599cd45b3ae0ca04
Justin <theskaterjesus@gmail.com>
2023-08-08 22:34:21 +0100

    Updated tests but config set not working

--- tests/Unit/RouteTest.php
+++ tests/Unit/RouteTest.php
@@ -1,9 +1,25 @@
 <?php
 
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Route;
+
 it('has a route for every route in the routes array in the config file', function () {
-    //
-})->todo();
+    $route = [
+        'name' => 'documentation',
+        'url' => '/documentation',
+        'dir' => 'docs/',
+        'middleware' => [
+            'web',
+        ],
+    ];
+
+    Config::set('lara-docs-kit.routes', [$route]);
+
+    expect(Route::has('lara-docs-kit.documentation'))->toBeTrue();
+});
 
 it('has no routes if the config file it empty', function () {
-    //
-})->todo();
+    Config::set('lara-docs-kit.routes', []);
+
+    expect(Route::has('lara-docs-kit.docs'))->toBeFalse();
+});
