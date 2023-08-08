129dda387961ad5b937fb99639b2ff3953dd701a
Justin <theskaterjesus@gmail.com>
2023-08-08 20:57:52 +0100

    Created blank tests for routes

---
+++ tests/Unit/RouteTest.php
@@ -0,0 +1,9 @@
+<?php
+
+it('has a route for every route in the routes array in the config file', function () {
+    //
+})->todo();
+
+it('has no routes if the config file it empty', function () {
+    //
+})->todo();
