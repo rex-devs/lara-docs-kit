f1512c80be9ed5b07ceee75438884f79b7a15d98
Justin <theskaterjesus@gmail.com>
2023-08-10 18:43:38 +0100

    updated test to check current docs

--- tests/Unit/RouteTest.php
+++ tests/Unit/RouteTest.php
@@ -1,25 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Route;
 
 it('has a route for every route in the routes array in the config file', function () {
-    $route = [
-        'name' => 'documentation',
-        'url' => '/documentation',
-        'dir' => 'docs/',
-        'middleware' => [
-            'web',
-        ],
-    ];
-
-    Config::set('lara-docs-kit.routes', [$route]);
-
-    expect(Route::has('lara-docs-kit.documentation'))->toBeTrue();
-});
-
-it('has no routes if the config file is empty', function () {
-    Config::set('lara-docs-kit.routes', []);
-
-    expect(Route::has('lara-docs-kit.docs'))->toBeFalse();
+    expect(Route::has('lara-docs-kit.docs'))->toBeTrue();
 });
