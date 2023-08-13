1fcd9c35b2e4adacdca79d343216f9e404c569b3
Justin <theskaterjesus@gmail.com>
2023-08-13 22:29:32 +0100

    Added comments to the config file

--- config/lara-docs-kit.php
+++ config/lara-docs-kit.php
@@ -1,6 +1,19 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Defining routes configuration
+    |--------------------------------------------------------------------------
+    |
+    | This array contains routes that provide easy access to the user documentation,
+    | allowing users to quickly find and reference essential information about
+    | the application's features and usage. These routes are designed to enhance
+    | user experience by offering clear and accessible documentation directly
+    | within the application.
+    |
+    */
+
     'routes' => [
         [
             'name' => 'docs',
@@ -11,6 +24,18 @@ return [
             ],
         ],
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Syntax Highlighting Theme Configuration
+    |--------------------------------------------------------------------------
+    |
+    | This configuration option allows you to specify the syntax highlighting
+    | theme for code blocks within your application's documentation.
+    | Choosing an appropriate theme enhances the readability and aesthetics
+    | of code examples for your developers and users.
+    |
+    */
 
     'highlight_theme' => env('LDK_HIGHLIGHT_THEME', 'dark-plus'),
 ];
