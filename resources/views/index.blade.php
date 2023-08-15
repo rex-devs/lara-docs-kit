78e3bf0812402fbb8686c856afdbbd5acb100774
Justin <theskaterjesus@gmail.com>
2023-08-15 22:31:09 +0100

    Created a basic sample of the docs page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,9 +9,43 @@
         <script src="https://cdn.tailwindcss.com?plugins=typography"></script>
     </head>
 
-    <body>
-        <div class="mx-auto prose lg:prose-xl">
-            {!! $html !!}
+    <body class="flex min-h-full bg-white antialiased dark:bg-zinc-900">
+        <div class="h-full lg:ml-72 xl:ml-80">
+            <header class="contents lg:pointer-events-none lg:fixed lg:inset-0 lg:z-40 lg:flex">
+                <div class="contents lg:pointer-events-auto lg:block lg:w-72 lg:overflow-y-auto lg:border-r lg:border-zinc-900/10 lg:px-6 lg:pb-8 lg:pt-4 lg:dark:border-white/10 xl:w-80">
+                    <div class="hidden lg:flex">
+                        <a aria-label="Home" href="/">
+                            Logo
+                        </a>
+                    </div>
+                    
+                    <nav class="hidden lg:mt-10 lg:block">
+                        <ul role="list">
+                            <li>
+                                <a class="block py-1 text-sm text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white" href="/">API</a>
+                            </li>
+
+                            <li>
+                                <a class="block py-1 text-sm text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white" href="#">Documentation</a>
+                            </li>
+
+                            <li>
+                                <a class="block py-1 text-sm text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white" href="#">Support</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+        </div>
+
+        <div class="relative flex h-full flex-col px-4 sm:px-6 lg:px-8 w-full">
+            <main class="flex-auto">
+                <article class="flex h-full flex-col pb-10 pt-16">
+                    <div class="w-full max-w-5xl mx-auto prose dark:prose-invert">
+                        {!! $html !!}
+                    </div>
+                </article>
+            </main>
         </div>
     </body>
 </html>
