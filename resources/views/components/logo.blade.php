790e3a1a3f66d40a8d8822ed31d0a58ab6a259e3
Justin <theskaterjesus@gmail.com>
2023-09-02 09:05:09 +0100

    updated logo to have a different size

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,3 +1,8 @@
-<div class="h-20 w-20 rounded-full overflow-hidden">
-    <img src="https://avatars.githubusercontent.com/u/140857883" alt="" />
+@props([
+    'h' => 'h-20',
+    'w' => 'w-20',
+])
+
+<div @class(['rounded-full overflow-hidden', $h, $w])>
+    <img src="https://avatars.githubusercontent.com/u/140857883" alt="" class="w-full h-full" />
 </div>
