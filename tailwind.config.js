52f72ddeabf61c6622731bf8ecfe5c27f15d489c
Justin <theskaterjesus@gmail.com>
2023-09-02 22:02:51 +0100

    Added tailwind css files

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/views/**/*.blade.php",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
+
