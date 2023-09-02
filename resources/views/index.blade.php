65d92bbb4d5b8ab633a344e53cedc28d0dbba727
Justin <theskaterjesus@gmail.com>
2023-09-02 10:59:53 +0100

    Added a mobile menu

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,7 +19,33 @@
                         </a>
                     </div>
 
-                    <x-lara-docs-kit::nav :navItems="$navItems" />
+                    <div class="fixed inset-x-0 top-0 z-50 flex h-14 items-center justify-between gap-12 px-4 transition sm:px-6 lg:left-72 lg:z-30 lg:px-8 xl:left-80 backdrop-blur-sm dark:backdrop-blur lg:left-72 xl:left-80 bg-white/[var(--bg-opacity-light)] dark:bg-zinc-900/[var(--bg-opacity-dark)] lg:hidden" style="--bg-opacity-light: 0.5; --bg-opacity-dark: 0.2;">
+                        <div class="flex items-center gap-5 lg:hidden">
+                            <button type="button" class="flex h-6 w-6 items-center justify-center rounded-md transition hover:bg-zinc-900/5 dark:hover:bg-white/5" onclick="document.getElementById('mobile-nav').classList.toggle('hidden'); document.documentElement.classList.toggle('overflow-hidden')" aria-label="Toggle navigation">
+                                <svg viewBox="0 0 10 9" fill="none" stroke-linecap="round" aria-hidden="true" class="w-2.5 stroke-zinc-900 dark:stroke-white">
+                                    <path d="M.5 1h9M.5 8h9M.5 4.5h9"></path>
+                                </svg>
+                            </button>
+                            
+                            <a aria-label="Home" href="/">
+                                <x-lara-docs-kit::logo h="h-6" w="w-auto" />
+                            </a>
+                        </div>
+                    </div>
+                    
+                    <div id="mobile-nav" class="z-10 hidden fixed bottom-0 left-0 top-14 w-full overflow-y-auto bg-white px-4 pb-4 pt-6 shadow-lg shadow-zinc-900/10 ring-1 ring-zinc-900/7.5 dark:bg-zinc-900 dark:ring-zinc-800 min-[416px]:max-w-sm sm:px-6 sm:pb-10 translate-x-0" data-projection-id="128">
+                        <nav>
+                            <x-lara-docs-kit::nav :navItems="$navItems" />
+                        </nav>
+                    </div>
+                
+                    <nav class="hidden lg:mt-10 lg:block">
+                        <div class="relative mt-3 pl-2">
+                            <div class="absolute inset-y-0 left-2 w-px bg-zinc-900/10 dark:bg-white/5"></div>
+
+                            <x-lara-docs-kit::nav :navItems="$navItems" />
+                        </div>
+                    </nav>
                 </div>
             </header>
         </div>
